236dbb4f369eef7154dd942245610e998339f374
JM Revagorda <jmcr011504@gmail.com>
2026-03-22 14:09:41 +0800

    Platformer v4.6a

--- Assets/Scripts/EndLevelScript.cs
+++ Assets/Scripts/EndLevelScript.cs
@@ -5,22 +5,29 @@ using UnityEngine.SceneManagement;
 public class EndLevelScript : MonoBehaviour
 {
     [SerializeField] GameObject cutsceneManager;
+    [SerializeField] GameObject levelLoader;
     [SerializeField] string SceneName;
     CutsceneManager csManager = null;
-
+    [SerializeField] bool canDoEndScene = false;
     void Awake() {
         cutsceneManager = GameObject.Find("CutsceneManager");
         csManager = cutsceneManager.GetComponent<CutsceneManager>();
+    }
+    public void enableEndScene()
+    {
+        canDoEndScene = true;
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             other.gameObject.GetComponent<PlayerMovement>().enabled = false;
-            csManager.SetSceneName(SceneName);
-            csManager.EndLevelScene();
+            if(canDoEndScene)
+                csManager.EndLevelScene();
+            else
+                levelLoader.GetComponent<LevelLoader>().LoadNextScene();
+            
         }
-            //StartCoroutine(LoadNextScene(SceneName));   //Starts the Coroutine if Player enters
     }
 
     //IEnumerator StopPlayer(string sceneName) {
