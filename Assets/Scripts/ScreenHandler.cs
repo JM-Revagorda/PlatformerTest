a695ea9dfe846dd5c32c2e6d4d5f251251ebad89
JM Revagorda <jmcr011504@gmail.com>
2026-03-23 21:10:59 +0800

    Platformer v4.7
    dealing with level 2 rn. Some Colliders arent supposed to be there

--- Assets/Scripts/ScreenHandler.cs
+++ Assets/Scripts/ScreenHandler.cs
@@ -14,8 +14,7 @@ public class ScreenHandler : MonoBehaviour
             {
                 isPaused = true;
                 EnableMainSettings();
-            } else
-            {
+            } else {
                 isPaused = false;
                 DisableMainSettings();
             }
@@ -41,13 +40,13 @@ public class ScreenHandler : MonoBehaviour
     public void EnableMainSettings()
     {
         mainSettings.SetActive(true);
-        gameObject.SetActive(false);
+        //gameObject.SetActive(false);
         PausePhysics();
     }
     public void DisableMainSettings()
     {
         mainSettings.SetActive(false);
-        gameObject.SetActive(true);
+        //gameObject.SetActive(true);
         ResumePhysics();
     }
 }
